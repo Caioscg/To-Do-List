184ee900bce32c7bff8c6a87821b0fb3c8e73f21
Caioscg <caio.gonzaga@discente.ufg.br>
2022-11-19 13:20:41 -0300

    submit button and autocomplete off

--- main.js
+++ main.js
@@ -4,10 +4,14 @@ mondayPlus.addEventListener("click", () => {
     `
     <li>
     <form action="">
-        <input type="text" placeholder="Digite sua atividade" id="name" style="background-color: #338af3; border: 0;">
+        <input type="text" placeholder="Digite sua atividade" id="name" autocomplete="off" style="background-color: #338af3; border: 0;">
     </form>
-    <input type="submit" value="✔️" style="background-color: #338af3; border: 0; cursor: pointer;">
+    <input type="submit" value="✔️" id="submit" style="background-color: #338af3; border: 0; cursor: pointer;">
     </li>
     `
     mondayPlus.style.visibility = "hidden"
+    document.querySelector("#submit").addEventListener("click", () => {
+        mondayPlus.style.visibility = "visible"
+        document.querySelector("#submit").style.visibility = "hidden"
+    })
 })
