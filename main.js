68effdedf041136e5ceb7e82b2530e4637bc8e32
Caioscg <caio.gonzaga@discente.ufg.br>
2022-11-21 16:33:38 -0300

    input fixes

--- main.js
+++ main.js
@@ -1,17 +1,44 @@
 const mondayPlus = document.querySelector("#monday-plus")
+let mondayListContent = ""
 mondayPlus.addEventListener("click", () => {
+    let i = 0
+    i++;
     const mondayUl = document.querySelector("#mondayUl").innerHTML = 
     `
+    ${mondayListContent}
     <li>
+
     <form action="">
-        <input type="text" placeholder="Digite sua atividade" id="name" autocomplete="off" style="background-color: #338af3; border: 0;">
+        <input type="text" placeholder="Digite sua atividade" id="name${i}" autocomplete="off" style="background-color: #338af3; border: 0;">
     </form>
-    <input type="submit" value="✔️" id="submit" style="background-color: #338af3; border: 0; cursor: pointer;">
+    
     </li>
     `
+
     mondayPlus.style.visibility = "hidden"
-    document.querySelector("#submit").addEventListener("click", () => {
+    const mondayCheck = document.querySelector("#submit")
+    mondayCheck.style.visibility = "visible"
+    mondayCheck.addEventListener("click", () => {
+        const form = document.querySelector("form")
+
+        console.log(form.name.value)  //? como passar o names${i} ?
+
+        //TODO: tentar fazer funcionar assim depois
+        mondayListContent += `
+        <li>
+   
+        <form action="">
+            <input type="text" placeholder="Digite sua atividade" autocomplete="off" style="background-color: #338af3; border: 0;" value="${form.name.value}">
+        </form>
+        
+        </li>
+        ` 
+        
+        console.log(mondayListContent)
+
+
         mondayPlus.style.visibility = "visible"
         document.querySelector("#submit").style.visibility = "hidden"
     })
+    
 })
