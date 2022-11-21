a33d6f9f22868b8ce57d59478ebbae3f5a713709
Caioscg <caio.gonzaga@discente.ufg.br>
2022-11-21 18:53:04 -0300

    input bug fix

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const mondayPlus = document.querySelector("#monday-plus")
-let mondayListContent = ""
+let mondayListContent = []
+let i = 0
+
 mondayPlus.addEventListener("click", () => {
-    let i = 0
     i++;
+    let j = i;
     const mondayUl = document.querySelector("#mondayUl").innerHTML = 
     `
-    ${mondayListContent}
+    ${mondayListContent.join("")}
     <li>
 
     <form action="">
@@ -19,16 +21,20 @@ mondayPlus.addEventListener("click", () => {
     const mondayCheck = document.querySelector("#submit")
     mondayCheck.style.visibility = "visible"
     mondayCheck.addEventListener("click", () => {
-        const form = document.querySelector("form")
 
-        console.log(form.name.value)  //? como passar o names${i} ?
+        let input = document.querySelector(`#name${i}`).value
+
+        let id = `name${i}`
+
+        console.log(input)  //? como passar o names${i} ?
+
 
         //TODO: tentar fazer funcionar assim depois
-        mondayListContent += `
+        mondayListContent[i] = `
         <li>
    
         <form action="">
-            <input type="text" placeholder="Digite sua atividade" autocomplete="off" style="background-color: #338af3; border: 0;" value="${form.name.value}">
+            <input type="text" placeholder="Digite sua atividade" autocomplete="off" style="background-color: #338af3; border: 0;" value="${input}">
         </form>
         
         </li>
